68e216453cadbc69a9a8b22c6df09ce4345c8a47
Logan M Hood <hoodlma@gmail.com>
2013-11-14 17:04:15 -0500

    Added a cover for genitals, enabled by default
    Former-commit-id: dc7fa4534f10732771fbc62038735bb43ce33ff1

---
+++ Assets/GUI/HideGenitals.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideGenitals : MonoBehaviour {
+	
+	public bool censoring = true;
+	
+	// Use this for initialization
+	void Start () {
+		gameObject.renderer.enabled = censoring;
+		gameObject.renderer.material.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.X)) {
+			gameObject.renderer.enabled = !censoring;
+			censoring = !censoring;
+		}
+	}
+}

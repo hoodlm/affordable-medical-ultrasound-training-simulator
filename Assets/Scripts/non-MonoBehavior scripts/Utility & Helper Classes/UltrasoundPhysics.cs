b9d92339addffac3e6323f266fe2eb39f7258133
Logan M Hood <hoodlma@gmail.com>
2013-11-14 22:01:07 -0500

    OPTIMIZED RAYCASTING 2X PERFORMANCE BOOST WOOOOOO
    Former-commit-id: 5733896903034fb8eb05907397965321566e562f

--- Assets/Scripts/non-MonoBehavior scripts/Utility & Helper Classes/UltrasoundPhysics.cs
+++ Assets/Scripts/non-MonoBehavior scripts/Utility & Helper Classes/UltrasoundPhysics.cs
@@ -5,18 +5,10 @@ using System.Collections;
 /// Various helper methods for simulating ultrasound pulses.
 /// </summary>
 public static class UltrasoundPhysics {
-	
 
-	// When trying to determine possible organs at a certain point,
-	// this is the minimum distance. Any colliders with a bounding box
-	// in this radius will be included. This is largely an arbitrary
-	// magic number based on rough performance tests. If it is any smaller,
-	// larger organs may appear to have holes in the middle of them.
-	private static readonly float ORGAN_THRESHOLD_DISTANCE = 0.178f;
-	
 	// The process of finding organs in a radius is expensive, so we cache this and only check it periodically.
 	private static Collider[] cachedPossibleOrgans;
-	private static Vector3 cachedPosition;
+	private static UltrasoundPulse cachedPulse;
 	
 	/// <summary>
 	/// Sends a pulse to a single point and returns information about that point.
@@ -24,7 +16,7 @@ public static class UltrasoundPhysics {
 	/// of the probe, since information about the probe's orientation is necessary for projection.
 	/// </summary>
 	public static UltrasoundPoint SendPulse(UltrasoundPulse pulse) {
-		Collider[] organs = FindPossibleOrgans(pulse.GetTarget());
+		Collider[] organs = FindPossibleOrgans(pulse);
 		foreach (Collider collider in organs) {
 			if (UltrasoundCollisionUtils.IsContained(pulse.GetTarget(), collider)) {
 				UltrasoundOrgan organ = GetOrganFromCollider(collider);
@@ -53,10 +45,15 @@ public static class UltrasoundPhysics {
 	/// <summary>
 	/// Finds the possible organs at a point.
 	/// </summary>
-	private static Collider[] FindPossibleOrgans(Vector3 point) {
-		if (null == cachedPossibleOrgans || Vector3.Distance(point, cachedPosition) > ORGAN_THRESHOLD_DISTANCE) {
-			cachedPossibleOrgans = Physics.OverlapSphere(point, ORGAN_THRESHOLD_DISTANCE);
-			cachedPosition = point;
+	private static Collider[] FindPossibleOrgans(UltrasoundPulse pulse) {
+		if (null == cachedPossibleOrgans || !pulse.Equals(cachedPulse)) {
+			RaycastHit[] hits = 
+				Physics.RaycastAll(pulse.GetOrigin(), pulse.GetTarget() - pulse.GetOrigin(), pulse.GetLength());
+			cachedPossibleOrgans = new Collider[hits.Length];
+			for (int i = 0; i < hits.Length; ++i) {
+				cachedPossibleOrgans[i] = hits[i].collider;
+			}
+			cachedPulse = pulse;
 		}
 		
 		return cachedPossibleOrgans;

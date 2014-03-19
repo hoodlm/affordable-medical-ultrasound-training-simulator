89e5d1b8fa9820cf04df2e051b80db0a7825d7f4
hoodlm <hoodlma@gmail.com>
2014-03-19 02:15:34 -0400

    Improved the 'naive' leap scene a bit
    Former-commit-id: 25b6a98b198dcf15523a8c551fe33c2815d7f5c6

--- Assets/Scripts/Input/LeapProbeMovement.cs
+++ Assets/Scripts/Input/LeapProbeMovement.cs
@@ -2,16 +2,39 @@
 using System.Collections;
 
 public class LeapProbeMovement : MonoBehaviour {
+
+	/// Singleton object to provide Leap input data.
 	private LeapManager _leapManager;
-	// Use this for initialization
+
+	/// The skin GameObject.
+	private GameObject skin;
+
+	/// Use this for initialization
 	void Start () {
 		_leapManager = (GameObject.Find("LeapManager")as GameObject).GetComponent(typeof(LeapManager)) as LeapManager;
+		UltrasoundDebug.Assert (null != _leapManager,
+		                        "Could not find LeapManager object. Add a LeapManager prefab to this scene.",
+								this);
+
+		skin = GameObject.FindGameObjectWithTag("Skin");
+		UltrasoundDebug.Assert(null != skin, 
+		                       "Could not find Skin object. Did you forget to tag it?",
+		                       this);
+		UltrasoundDebug.Assert(null != skin.collider, 
+		                       "Skin object does not have a collider.",
+		                       this);
 	}
 	
-	// Update is called once per frame
+	/// Update is called once per frame
 	void Update () {
+		UpdateLeapLocation();
+		KeepOnSkin();
+	}
+
+	/// Retrieves location data from the Leap controller, if available, and moves the object to the appropriate
+	/// position.
+	private void UpdateLeapLocation() {
 		if(_leapManager != null) { 
-			OnionLogger.globalLog.LogInfo("asdf");
 			if(_leapManager.pointerAvailible)
 			{
 				this.transform.position = _leapManager.pointerPositionWorld;
@@ -20,6 +43,18 @@ public class LeapProbeMovement : MonoBehaviour {
 			else
 			{
 				renderer.enabled = false;
+			}
+		}
+	}
+
+	/// Prevents the probe from passing through the surface of the patient's skin.
+	private void KeepOnSkin() {
+		if (UltrasoundCollisionUtils.IsContained(this.transform.position, skin.collider)) {
+			Vector3 rayStart = transform.position - transform.forward * 30f;
+			Ray ray = new Ray(rayStart, transform.forward);
+			RaycastHit hit;
+			if (skin.collider.Raycast(ray, out hit, float.PositiveInfinity)) {
+				this.transform.position = hit.point;
 			}
 		}
 	}
